bf41d8bbe39d505d0526499b56f5f2670701c874
Eric Dalessio <edalessio@gmail.com>
2024-10-21 13:52:50 -0400

    added calendar funcitonality

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -59,6 +59,112 @@ const router = express.Router();
 let records = [];
 
 
+/*
+ * Configure CORS
+ */
+app.use(cors());
+
+app.listen(HTTP_PORT, (error) => {
+  if (!error) {
+    logger.info("Server running on port: " + HTTP_PORT);
+    // Cache the first events after server startup
+    cacheEvents();
+  }
+  else
+    logger.error("Error in starting HTTP Server", error);
+}
+);
+app.get('/events', (request, response) => {
+  response.status(200);
+  response.set(HEADER_CONTENT_TYPE, MIME_TYPE_JSON);
+  response.send(getEvents());
+});
+
+// Set up cache refresh
+setInterval(cacheEvents, SECONDS_BETWEEN_CACHE_REFRESH * 1000);
+
+// Constants
+const { google } = require('googleapis');
+const cal = google.calendar({
+  version: 'v3',
+  auth: API_KEY
+});
+
+/**
+ * Returns the array of calendar events; if not yet defined, attains these and caches them
+ * 
+ * @returns The array of calendar events
+ */
+function getEvents() {
+  if (calendarEvents === undefined) {
+    cacheEvents();
+  }
+  return calendarEvents;
+}
+
+/**
+ * Fetch and cache the latest calendar events
+ */
+function cacheEvents() {
+  // Pre-fetch the first calendar events
+  fetchEvents().then(
+    response => {
+      calendarEvents = response;
+      logger.debug(JSON.stringify(calendarEvents));
+      logger.info('Refreshed Cache');
+    }
+  )
+}
+
+/**
+ * Lists the upcoming events for CALENDAR_ID
+ *  
+ * @return An JSON representation of calendar events, each event with keys: start, end, summary, hangoutLink, htmlLink, location, description
+ */
+async function fetchEvents() {
+  const calendar = cal;
+  const calEvents = [];
+  const res = await calendar.events.list({
+    calendarId: CALENDAR_ID,
+    timeMin: new Date().toISOString(),
+    maxResults: NUM_EVENTS_TO_FETCH,
+    singleEvents: true,
+    orderBy: 'startTime',
+  });
+  const events = res.data.items;
+  if (!events || events.length === 0) {
+    logger.debug('No upcoming events found.');
+    return calEvents;
+  }
+  logger.debug('Next (max) ' + NUM_EVENTS_TO_FETCH + ' Events:');
+  events.map((event, i) => {
+
+    // Hold the object
+    const calEvent = new Object();
+    calEvent.start = event.start.dateTime || event.start.date;
+    calEvent.end = event.end.dateTime || event.end.date;
+    calEvent.summary = event.summary;
+    calEvent.hangoutLink = event.hangoutLink;
+    calEvent.htmlLink = event.htmlLink;
+    calEvent.location = event.location;
+    calEvent.description = event.description;
+
+    calEvents.push(calEvent);
+
+    const eventDetail = "\nSummary: " + calEvent.summary + "\nHangout Link: " +
+      calEvent.hangoutLink + "\nHTML Link: " +
+      calEvent.htmlLink + "\nLocation: " +
+      calEvent.location + "\nStart: " +
+      calEvent.start + "\nEnd: " +
+      calEvent.end + "\nDescription: " +
+      calEvent.description + "\n"
+    logger.debug(eventDetail);
+  });
+  return JSON.stringify(calEvents);
+}
+
+
+
 router.get('/', (req, res) => {
   res.send('App is running..');
 });
